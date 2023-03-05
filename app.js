84cb4a126f4bfb5461bdd81cfcaf98a99e5ced0d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-03-05 22:46:11 +0300

    Connected to firebase

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Initialize Firebase(2)
-    const firebaseConfig = {
-        apiKey: "AIzaSyD2_SPlvBtz1uMfALDHezMYb5sjikRDAU8",
-        authDomain: "https://loginform-85e86-default-rtdb.firebaseio.com/",
-        projectId: "loginform-85e86",
-        storageBucket: "loginform-85e86.appspot.com",
-        messagingSenderId: "345225757235",
-        appId: "1:345225757235:web:956f827ba8575fbbfbd946",
-        measurementId: "G-K9XJNJKCMP"
-      };
+  const firebaseConfig = {
+    apiKey: "AIzaSyD2_SPlvBtz1uMfALDHezMYb5sjikRDAU8",
+    authDomain: "loginform-85e86.firebaseapp.com",
+    databaseURL: "https://loginform-85e86-default-rtdb.firebaseio.com",
+    projectId: "loginform-85e86",
+    storageBucket: "loginform-85e86.appspot.com",
+    messagingSenderId: "345225757235",
+    appId: "1:345225757235:web:956f827ba8575fbbfbd946",
+    measurementId: "G-K9XJNJKCMP"
+  };
 firebase.initializeApp(firebaseConfig);
 
 //Reference for form collection(3)
@@ -20,12 +21,12 @@ document.getElementById('registrationform').addEventListener('submit', formSubmi
 function formSubmit(e) {
   e.preventDefault();
   // Get Values from the DOM
-  var name = document.querySelector('#name').value;
-  var email = document.querySelector('#email').value;
-  var password = document.querySelector('#password').value;
-  var bio = document.querySelector('#bio').value;
-  var job = document.querySelector('#job').value;
-  var interest = document.querySelector('#interest').value;
+  var name = getElementVal('name');
+  var email = getElementVal('email');
+  var password = getElementVal('password');
+  var bio = getElementVal('bio');
+  var job = getElementVal('job');
+  var interest = getElementVal('interest');
 
   console.log(name, email, password, bio, job, interest);
 
@@ -35,9 +36,9 @@ function formSubmit(e) {
   document.querySelector('.alert').style.display = 'block';
 
   //Hide Alert Message After Seven Seconds(6)
-  setTimeout(function() {
+  setTimeout(() => {
     document.querySelector('.alert').style.display = 'none';
-  }, 7000);
+  }, 3000);
 
   //Form Reset After Submission(7)
   document.getElementById('registrationform').reset();
@@ -46,6 +47,7 @@ function formSubmit(e) {
   //send message values
  const sendMessage = (name, email, password, bio, job, interest) =>{
   var newFormMessage = formMessage.push();
+
   newFormMessage.set({
     name: name,
     email: email,
@@ -55,6 +57,10 @@ function formSubmit(e) {
     Interest: interest
   });
 }
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
 //Send Message to Firebase(4)
 
 // function sendMessage(name, email, password, bio, job, interest) {
